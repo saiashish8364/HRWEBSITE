ba917aa528bc64351b5de6fa22582b0e17bc0f81
saiashish8364 <polusaiashish97@gmail.com>
2025-11-10 22:39:58 +0530

    3commit

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Services from "./pages/Services";
-import AboutPage from "./pages/AboutPage";
-import CareersPage from "./pages/CareersPage";
-import ContactPage from "./pages/ContactPage";
-import BlogPost from "./pages/BlogPost";
-import NotFound from "./pages/NotFound";
+import Home from "@/pages/Home";
+import Services from "@/pages/Services";
+import AboutPage from "@/pages/AboutPage";
+import CareersPage from "@/pages/CareersPage";
+import ContactPage from "@/pages/ContactPage";
+import BlogPost from "@/pages/BlogPost";
+import NotFound from "@/pages/NotFound";
 
 const queryClient = new QueryClient();
 
